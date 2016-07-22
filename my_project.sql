cdc1a8507106d9e753921c11552fe72ac31c9bd8
adisurya23 <adisurya75@gmail.com>
2016-07-22 12:44:41 -0700

    Add files via upload

--- my_project.sql
+++ my_project.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `belt_exam_prep` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `belt_exam_prep`;
+CREATE DATABASE  IF NOT EXISTS `pylot_belt_exam` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `pylot_belt_exam`;
 -- MySQL dump 10.13  Distrib 5.7.9, for osx10.9 (x86_64)
 --
--- Host: 127.0.0.1    Database: belt_exam_prep
+-- Host: 127.0.0.1    Database: pylot_belt_exam
 -- ------------------------------------------------------
 -- Server version	5.5.42
 
@@ -18,88 +18,32 @@ USE `belt_exam_prep`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `authors`
+-- Table structure for table `quotes`
 --
 
-DROP TABLE IF EXISTS `authors`;
+DROP TABLE IF EXISTS `quotes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `authors` (
+CREATE TABLE `quotes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
+  `content` varchar(255) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `authors`
---
-
-LOCK TABLES `authors` WRITE;
-/*!40000 ALTER TABLE `authors` DISABLE KEYS */;
-/*!40000 ALTER TABLE `authors` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `books`
---
-
-DROP TABLE IF EXISTS `books`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `books` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(45) DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  `authors_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_books_authors1_idx` (`authors_id`),
-  CONSTRAINT `fk_books_authors1` FOREIGN KEY (`authors_id`) REFERENCES `authors` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `books`
---
-
-LOCK TABLES `books` WRITE;
-/*!40000 ALTER TABLE `books` DISABLE KEYS */;
-/*!40000 ALTER TABLE `books` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `reviews`
---
-
-DROP TABLE IF EXISTS `reviews`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `reviews` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `contents` varchar(255) DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  `ratings` int(11) DEFAULT NULL,
   `users_id` int(11) NOT NULL,
-  `books_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_reviews_users_idx` (`users_id`),
-  KEY `fk_reviews_books1_idx` (`books_id`),
-  CONSTRAINT `fk_reviews_users` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_reviews_books1` FOREIGN KEY (`books_id`) REFERENCES `books` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  KEY `fk_quotes_users_idx` (`users_id`),
+  CONSTRAINT `fk_quotes_users` FOREIGN KEY (`users_id`) REFERENCES `mydb`.`users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `reviews`
+-- Dumping data for table `quotes`
 --
 
-LOCK TABLES `reviews` WRITE;
-/*!40000 ALTER TABLE `reviews` DISABLE KEYS */;
-/*!40000 ALTER TABLE `reviews` ENABLE KEYS */;
+LOCK TABLES `quotes` WRITE;
+/*!40000 ALTER TABLE `quotes` DISABLE KEYS */;
+INSERT INTO `quotes` VALUES (3,'Today is wonderful','2016-07-22 10:53:17',NULL,1),(5,'Tomorrow is Saturday','2016-07-22 11:27:04',NULL,1);
+/*!40000 ALTER TABLE `quotes` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -118,7 +62,7 @@ CREATE TABLE `users` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -127,7 +71,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'jghjhg','hjghjhg','jhhj@jdjf.com','$2b$12$SW0ZxcTBQFRVnunA0duczuP7RuufJ/H74yOZDqF3u/pxZFjnErPx.','2016-07-21 17:18:56','2016-07-21 17:18:56');
+INSERT INTO `users` VALUES (1,'selamet','adi','adi@surya.com','$2b$12$NaTsYJbtvTF3AFqBgC0b6O4SBB7e8f04NIXpPlpow.KtifHJTnNg6','2016-07-22 09:48:56','2016-07-22 09:48:56'),(2,'caitlin','cait','cait@surya.com','$2b$12$.4lEvE4WEiDbVRXQ/DyN2Ok7QZl2U5enflZfrKHGn.6E76vjyGFW2','2016-07-22 09:51:38','2016-07-22 09:51:38'),(3,'caitlin','cait','cait@surya.com','$2b$12$VF0ys0UosHUfrg0B9SHHOuxYb2UrRGJ6scy.pcbcvJPgxg9d/r5Tu','2016-07-22 09:51:57','2016-07-22 09:51:57'),(4,'bing','bingbing','bing@bing.com','$2b$12$oY4uYzrwO1zlW9W8fxJAROcoWy14HZUZwUEr/U74HpaL1Ux0Pb9MK','2016-07-22 09:57:49','2016-07-22 09:57:49');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -140,4 +84,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-07-21 18:06:14
+-- Dump completed on 2016-07-22 12:38:02
